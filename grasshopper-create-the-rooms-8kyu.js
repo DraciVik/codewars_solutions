31b89acb623c095488fd07de890cfc70a0a3ff48
DraciVik <viktor.stojanov90@gmail.com>
2019-05-03 23:43:58 +0200

    Solved an 8kyu kata

---
+++ grasshopper-create-the-rooms-8kyu.js
@@ -0,0 +1,18 @@
+// Add rooms here
+const rooms = {
+        killingRoom: {
+                name: 'Killing Room',
+                description: 'People get killed here',
+                completed: false,
+        },
+        waitingRoom: {
+                name: 'Waiting Room',
+                description: 'People wait here',
+                completed: false,
+        },
+        runningRoom: {
+                name: 'Running Room',
+                description: 'People run here',
+                completed: false,
+        },
+};
