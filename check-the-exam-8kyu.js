283a905234ae33ef3bc286ec87b749e6b0287c44
DraciVik <viktor.stojanov90@gmail.com>
2019-05-09 23:50:18 +0200

    Solved an 8kyu kata

--- check-the-exam-8kyu.js
+++ check-the-exam-8kyu.js
@@ -2,6 +2,7 @@ function checkExam(array1, array2) {
         let counter = 0;
         for (let i = 0; i < array1.length; i += 1) {
                 if (array2[i] === '') {
+                        // eslint-disable-next-line no-continue
                         continue;
                 } else if (array1[i] === array2[i]) {
                         counter += 4;
