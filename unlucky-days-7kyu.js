44ee211deb1a1e0afadc0e13285bf944f512d699
DraciVik <viktor.stojanov90@gmail.com>
2019-05-09 23:58:06 +0200

    Added description to some katas I forgot to add to before

--- unlucky-days-7kyu.js
+++ unlucky-days-7kyu.js
@@ -1,3 +1,16 @@
+// Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+
+// Find the number of Friday 13th in the given year.
+
+// Input: Year as an integer.
+
+// Output: Number of Black Fridays in the year as an integer.
+
+// Examples:
+
+// unluckyDays(2015) == 3
+// unluckyDays(1986) == 1
+
 function unluckyDays(year) {
         let count = 0;
         for (let month = 0; month < 12; month++) {
