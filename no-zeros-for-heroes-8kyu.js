0accc7d6d2b9e55c7bb7e5b6179a841de8356827
DraciVik <viktor.stojanov90@gmail.com>
2019-05-14 00:23:23 +0200

    Solved an 8kyu kata

--- no-zeros-for-heroes-8kyu.js
+++ no-zeros-for-heroes-8kyu.js
@@ -10,3 +10,13 @@
 // -1050 -> -105
 
 // Zero alone is fine, don't worry about it. Poor guy anyway
+
+function noBoringZeros(n) {
+        const nString = n.toString().split('');
+        for (let i = 0; i <= nString.length; i += 1) {
+                if (nString[nString.length - 1] === '0') {
+                        nString.pop();
+                }
+        }
+        return Number(nString.join(''));
+}
