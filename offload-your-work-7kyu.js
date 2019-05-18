f24cf75bf71073df28eb8bff1d1dcb31a59c8009
DraciVik <viktor.stojanov90@gmail.com>
2019-05-18 18:25:38 +0200

    7kyu

---
+++ offload-your-work-7kyu.js
@@ -0,0 +1,23 @@
+// You are the best freelancer in the city. Everybody knows you, but what they don't know, is that you are actually offloading your work to other freelancers and and you rarely need to do any work. You're living the life!
+
+// To make this process easier you need to write a method called workNeeded to figure out how much time you need to contribute to a project.
+
+// Giving the amount of time in minutes needed to complete the project and an array of pair values representing other freelancers' time in [Hours, Minutes] format ie. [[2, 33], [3, 44]] calculate how much time you will need to contribute to the project (if at all) and return a string depending on the case.
+
+//     If we need to contribute time to the project then return "I need to work x hour(s) and y minute(s)"
+//     If we don't have to contribute any time to the project then return "Easy Money!"
+
+function workNeeded(projectMinutes, freelancers) {
+        let freelancersMinutes = 0;
+        const remainingMinutes = projectMinutes - freelancersMinutes;
+        for (let i = 0; i < freelancers.length; i += 1) {
+                freelancersMinutes += freelancers[i][0] * 60 + freelancers[i][1];
+        }
+        if (freelancersMinutes >= projectMinutes) {
+                return 'Easy Money!';
+        }
+
+        return `I need to work ${Math.floor(remainingMinutes / 60)} hour(s) and ${remainingMinutes % 60} minute(s)`;
+}
+
+// TODO - FIX!
