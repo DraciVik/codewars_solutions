c460f861c8d45938eb247601ef995f9dbb682e51
DraciVik <viktor.stojanov90@gmail.com>
2019-05-22 23:23:15 +0200

    Solved 'The Feast of Many Beasts' 8kyu kata

--- array-diff-6kyu.js
+++ array-diff-6kyu.js
@@ -8,18 +8,18 @@
 // array_diff([1,2,2,2,3],[2]) == [1,3]
 
 function array_diff(a, b) {
-  // goes trough all the items in array "a"
-  for (let i = 0; i < a.length; i++) {
-    // goes trough all the items in array "b"
-    for (let j = 0; j < b.length; j++) {
-      // checks if current item in array "a" is the same as item in array "b"
-      if (a[i] === b[j]) {
-        //if true it removes it from array "a"
-        a.splice(i, 1);
-        //because the item is removed the count for "i" needs to be set back one item
-        i--;
-      }
-    }
-  }
-  return a;
+        // goes trough all the items in array "a"
+        for (let i = 0; i < a.length; i++) {
+                // goes trough all the items in array "b"
+                for (let j = 0; j < b.length; j++) {
+                        // checks if current item in array "a" is the same as item in array "b"
+                        if (a[i] === b[j]) {
+                                // if true it removes it from array "a"
+                                a.splice(i, 1);
+                                // because the item is removed the count for "i" needs to be set back one item
+                                i--;
+                        }
+                }
+        }
+        return a;
 }
