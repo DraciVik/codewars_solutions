9834518e1c4a893bba41dbce6013f22f621115e2
DraciVik <viktor.stojanov90@gmail.com>
2019-06-05 23:41:55 +0200

    Solved 'All Star Code Challenge #3' 7kyu

---
+++ 7kyu-all-star-code-challenge-3.js
@@ -0,0 +1,13 @@
+// This Kata is intended as a small challenge for my students
+
+// Create a function, called removeVowels, that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+
+// removeVowels("drake") // => "drk"
+// removeVowels("aeiou") // => ""
+
+const removeVowels = function(str) {
+        return str
+                .split('')
+                .filter(s => s !== 'a' && s !== 'e' && s !== 'i' && s !== 'o' && s !== 'u')
+                .join('');
+};
