2f24224248ea3e910c0f3478086571ee3fc34648
DraciVik <viktor.stojanov90@gmail.com>
2019-06-11 23:49:27 +0200

    Solved 'Count Odd Numbers below n' 8kyu kata

---
+++ 8kyu-count-odd-numbers-below-n.js
@@ -0,0 +1,13 @@
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// oddCount(7) //=> 3, i.e [1, 3, 5]
+// oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+
+// Expect large Inputs!
+
+function oddCount(n) {
+        if (n % 2 === 1) {
+                return (n - 1) / 2;
+        }
+        return (n - 2) / 2 + 1;
+}
