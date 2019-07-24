f760c314e08cca2d4d46fc59990ff45bc3bf2db4
DraciVik <viktor.stojanov90@gmail.com>
2019-07-24 23:59:17 +0200

    Solved 'Sum of a Beach' 7kyu kata

---
+++ 7kyu-sum-of-a-beach.js
@@ -0,0 +1,16 @@
+// Beaches are filled with sand, water, fish, and sun. Given a string, calculate how many times the words "Sand", "Water", "Fish", and "Sun" appear without overlapping (regardless of the case).
+// Examples
+
+// sumOfABeach("WAtErSlIde")                    ==>  1
+// sumOfABeach("GolDeNSanDyWateRyBeaChSuNN")    ==>  3
+// sumOfABeach("gOfIshsunesunFiSh")             ==>  4
+// sumOfABeach("cItYTowNcARShoW")               ==>  0
+
+function sumOfABeach(beach) {
+        const regex = /sand|water|fish|sun/gi;
+        const repeatBeach = beach.match(regex);
+        if (repeatBeach === []) {
+                return 0;
+        }
+        return repeatBeach.length;
+}
