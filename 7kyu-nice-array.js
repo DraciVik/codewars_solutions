da9519867611780d8c4b86f8cdbad9e5928f54df
DraciVik <viktor.stojanov90@gmail.com>
2019-07-31 00:00:02 +0200

    Solved 'Nice Array' 7kyu kata

---
+++ 7kyu-nice-array.js
@@ -0,0 +1,29 @@
+// A Nice array is defined to be an array where for every value n in the array, there is also an element n-1 or n+1 in the array.
+
+// example:
+
+// [2,10,9,3] is Nice array because
+
+// 2=3-1
+// 10=9+1
+// 3=2+1
+// 9=10-1
+
+// Write a function named isNice/IsNice that returns true if its array argument is a Nice array, else false. You should also return false if input array has no elements.
+function isNice(arr) {
+        const result = [];
+        for (let i = 0; i < arr.length; i += 1) {
+                if (arr.includes(arr[i] - 1) || arr.includes(arr[i] + 1)) {
+                        result.push(true);
+                } else {
+                        result.push(false);
+                }
+        }
+        if (arr.length < 1) {
+                return false;
+        }
+        if (result.includes(false)) {
+                return false;
+        }
+        return true;
+}
