bd3e9c9fb890dc44729d2fa2b4e7af050da62069
DraciVik <viktor.stojanov90@gmail.com>
2019-08-02 22:54:08 +0200

    Solved 'Club Doorman' 7kyu kata

---
+++ 7kyu-club-doorman.js
@@ -0,0 +1,28 @@
+// INTRODUCTION
+
+// The Club Doorman will give you a word. To enter the Club you need to provide the right number according to provided the word.
+
+// Every given word has a doubled letter, like 'tt' in lettuce.
+
+// To answer the right number you need to find the doubled letter's position of the given word in the alphabet and multiply this number per 3.
+
+// It will be given only words with one doubled letter.
+
+// EXAMPLE
+
+// Lettuce is the given word. 't' is the doubled letter and it's position is 20 in the alphabet.
+
+// The answer to the Club Doorman is 20 * 3 = 60
+
+// TASK
+
+// The function passTheDoorMan with a given string word shall return the right number.
+
+function passTheDoorMan(word) {
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+        for (let i = 0; i < word.length; i += 1) {
+                if (word[i] == word[i + 1]) {
+                        return (alphabet.indexOf(word[i]) + 1) * 3;
+                }
+        }
+}
