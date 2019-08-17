2cbc01c1f17a025de5e49497880a781d7608610d
DraciVik <viktor.stojanov90@gmail.com>
2019-08-17 23:52:16 +0200

    Solved 'Printer Errors' 7kyu kata

---
+++ 7kyu-decrypt-this-school-cipher.js
@@ -0,0 +1,17 @@
+// School students Alice and Bob send messages to each other. To ensure that their messages are not readable by teachers they encrypt text with simple algorythm. Every message contains only latin letters (lowercase and uppercase), digits from 0 to 9 and space symbol.
+
+// So, the encryption algorythm is:
+
+//     Reverse the whole string.
+//     Replace every letter with it ASCII code in quotes (A to '65', h to '104' and so on).
+//     Insert digits and spaces as is.
+
+// E.g. this message:
+
+// 9Hi Alice4
+
+// is encrypted as
+
+// 4'101''99''105''108''65' '105''72'9
+
+// Your task is to write function decrypt to get source messages from encrypted strings.
