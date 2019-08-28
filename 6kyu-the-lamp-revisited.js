2b5aa810574d077b3324334dc0ef78395e9d2c1b
DraciVik <viktor.stojanov90@gmail.com>
2019-08-28 13:17:04 +0200

    Solved 'The Lamp:Revisited' 6kyu kata

---
+++ 6kyu-the-lamp-revisited.js
@@ -0,0 +1,22 @@
+// Define a class called Lamp. It will have a string attribute for color and boolean attribute, on, that will refer to whether the lamp is on or not. Define your class constructor with a parameter for color and assign on as false on initialize.
+
+// Give the lamp an instance method called toggle_switch that will switch the value of the on attribute.
+
+// Define another instance method called state that will return "The lamp is on." if it's on and "The lamp is off." otherwise.
+
+// // # write your code below
+
+class Lamp {
+        constructor(color) {
+                this.color = color;
+                this.on = false;
+        }
+
+        toggle_switch() {
+                this.on = !this.on;
+        }
+
+        state() {
+                return `The lamp is ${this.on ? 'on.' : 'off.'}`;
+        }
+}
