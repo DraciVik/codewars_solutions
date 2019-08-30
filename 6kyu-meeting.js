03370490cb65a5f165bbb38676c52a9ef1d90741
DraciVik <viktor.stojanov90@gmail.com>
2019-08-30 19:05:11 +0200

    Solved 'Primoral Of a Number' 6kyu kata

--- 6kyu-meeting.js
+++ 6kyu-meeting.js
@@ -25,15 +25,7 @@ function meeting(s) {
                 const newValue = value.split(':');
                 return newValue.reverse().join(', ');
         });
-        const sorted = lastNameFirst.sort((a, b) => {
-                if (a < b) {
-                        return -1;
-                }
-                if (a > b) {
-                        return 1;
-                }
-                return 0;
-        });
+        const sorted = lastNameFirst.sort();
         const myValues = sorted.map(value => `(${value})`);
         return myValues.join('');
 }
