9aa6dcfbe6201e167dfd41bdadc5fbc4fa3c38c8
DraciVik <viktor.stojanov90@gmail.com>
2019-09-06 01:34:09 +0200

    Solved 'A String of Sorts' 6kyu kata

---
+++ 6kyu-a-string-of-sorts.js
@@ -0,0 +1,28 @@
+// Define a method that accepts 2 strings as parameters. The method returns the first string sorted by the second.
+
+// sortString("foos", "of")       => "oofs"
+
+// sortString("string", "gnirts") => "gnirts"
+
+// sortString("banana", "abn")    => "aaabnn"
+
+// To elaborate, the second string defines the ordering. It is possible that in the second string characters repeat, so you should remove repeating characters, leaving only the first occurrence.
+
+// Any character in the first string that does not appear in the second string should be sorted to the end of the result in original order.
+
+function sortString(string, ordering) {
+
+    let strArr = string.split("");
+    let newArr = [];
+    let ordered = [];
+
+    for (let i = 0; i < ordering.length; i += 1) {
+        let filtered = strArr.filter(value => value === ordering[i]);
+        ordered = ordered.concat(filtered);
+        strArr = strArr.filter(value => value !== ordering[i])
+    }
+    ordered = ordered.concat(strArr);
+
+    return ordered.join("");
+
+}
