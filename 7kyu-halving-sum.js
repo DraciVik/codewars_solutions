8279504d361c94ea58a8d4394cdfa4dad880caca
DraciVik <viktor.stojanov90@gmail.com>
2020-07-02 22:20:52 +0200

    Initiated 7kyu-halving-sum

---
+++ 7kyu-halving-sum.js
@@ -0,0 +1,16 @@
+// Task
+
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+
+// All elements of the sum are the results of integer division.
+// Example
+
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+function halvingSum(n) {
+	let result;
+	//   for(let i = 0; i < Math.sqrt(n))
+}
+//  Just started
